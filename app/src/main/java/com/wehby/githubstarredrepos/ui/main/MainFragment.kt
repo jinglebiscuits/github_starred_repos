a3c644e7a5fb14824e629b3f201d31f223eb877c
jinglebiscuits <scott.wehby@gmail.com>
2021-05-12 19:49:52 -0500

    got a button

--- app/src/main/java/com/wehby/githubstarredrepos/ui/main/MainFragment.kt
+++ app/src/main/java/com/wehby/githubstarredrepos/ui/main/MainFragment.kt
@@ -1,26 +1,33 @@
 package com.wehby.githubstarredrepos.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.wehby.githubstarredrepos.R
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
+    private lateinit var textView: TextView
+    private lateinit var makeRequestButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        textView = view.findViewById(R.id.message)
+        makeRequestButton = view.findViewById(R.id.make_request_button)
+        makeRequestButton.setOnClickListener {
+            Log.d("MainFragemtn", "clicking")
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +36,7 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    companion object {
+        fun newInstance() = MainFragment()
+    }
 }

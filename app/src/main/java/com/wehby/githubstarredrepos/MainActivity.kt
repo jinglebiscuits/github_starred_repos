35dcfe16cc2f00df1837f456240b5517fa168a46
jinglebiscuits <scott.wehby@gmail.com>
2021-05-12 19:38:43 -0500

    big init

---
+++ app/src/main/java/com/wehby/githubstarredrepos/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.wehby.githubstarredrepos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wehby.githubstarredrepos.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}

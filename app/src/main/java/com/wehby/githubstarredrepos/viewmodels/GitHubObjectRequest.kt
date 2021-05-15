308e17e49f952eef6d066d8c37509448040f6ad5
jinglebiscuits <scott.wehby@gmail.com>
2021-05-15 15:17:23 -0500

    cleanup

--- app/src/main/java/com/wehby/githubstarredrepos/viewmodels/GitHubObjectRequest.kt
+++ app/src/main/java/com/wehby/githubstarredrepos/viewmodels/GitHubObjectRequest.kt
@@ -1,6 +1,5 @@
 package com.wehby.githubstarredrepos.viewmodels
 
-import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.wehby.githubstarredrepos.BuildConfig
@@ -11,10 +10,8 @@ class GitHubObjectRequest(url: String, listener: Response.Listener<JSONObject>, 
 
     override fun getHeaders(): MutableMap<String, String> {
         return if (BuildConfig.GITHUB_TOKEN == "") {
-            Log.d("TEST", "jedi no header")
             super.getHeaders()
         } else {
-            Log.d("TEST", "jedi header")
             val headers = HashMap<String, String>()
             headers["Authorization"] = BuildConfig.GITHUB_TOKEN
             headers
